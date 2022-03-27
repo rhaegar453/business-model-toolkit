bb91db49dfab398fe1447bfb0907e3744245930d
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-03-27 18:23:15 +0530

    Added the colors file

---
+++ src/utils/theme/index.ts
@@ -0,0 +1,10 @@
+const colors:Record<string, string>={
+    bg:'#041C32',
+    primary:'#51C4D3',
+    secondary:'#126E82', 
+    text_primary:'#D8E3E7',
+}
+
+
+export default colors;
+export type ColorTypes=keyof typeof colors;
