890ab8c143d8f3fdbb1ad2c81a382a03f1f2a12d
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-03-28 01:18:47 +0530

    Added the changes for the fielc component

--- src/components/field/Input/index.tsx
+++ src/components/field/Input/index.tsx
@@ -3,29 +3,38 @@ import styled from 'styled-components';
 import { FieldContext, MessageType } from '..';
 import colors from '../../../utils/theme';
 
-export interface InputProps extends HTMLAttributes<HTMLInputElement>{
-    variant?:MessageType
+export interface InputProps extends HTMLAttributes<HTMLInputElement> {
+  variant?: MessageType;
 }
 
-const getBorderStyles = ({ variant }:InputProps) => {
+const getBorderStyles = ({ variant }: InputProps) => {
   switch (variant) {
     case 'success':
       return `1px solid ${colors.primary}`;
     case 'error':
       return `1px solid ${colors.danger}`;
     default:
-      return '1px solid #e8e8e8';
+      return '1px solid #797779';
   }
 };
 
-const FieldBase:React.FC<InputProps> = styled.input`
-padding: 12px 12px;
-font-size: 15px;
-border: ${getBorderStyles};
-border-radius: 5px;
+const FieldBase: React.FC<InputProps> = styled.input`
+  padding: 12px 12px;
+  font-size: 15px;
+  border: ${getBorderStyles};
+  border-radius: 5px;
+  background-color: ${colors.ternary};
+  width: -webkit-fill-available;
+  color: white;
+  &::placeholder {
+    color: #959195;
+  }
+  &:focus {
+    outline: none;
+  }
 `;
 
-const Input:React.FC<InputProps> = (props) => {
+const Input: React.FC<InputProps> = (props) => {
   const { type } = useContext(FieldContext);
   return <FieldBase {...props} variant={type} />;
 };
--- src/utils/theme/index.ts
+++ src/utils/theme/index.ts
@@ -2,6 +2,7 @@ const colors = {
   bg: '#041C32',
   primary: '#51C4D3',
   secondary: '#126E82',
+  ternary: '#072E51',
   text_primary: '#D8E3E7',
   danger: '#EF617C',
 };
