01f8212a50bbdd4071e59125043ab5caf4e9d530
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-03-30 00:41:29 +0530

    Added the changes for the component

--- src/index.tsx
+++ src/index.tsx
@@ -7,3 +7,8 @@ export { default as ProjectItem } from './components/projectItem';
 export { default as Text } from './components/text';
 export { default as Accordian } from './components/accordian';
 export { default as Animations } from './components/Animations';
+
+/* Hooks */
+export { default as useDevice } from './utils/hooks/useDevice';
+export { default as useMediaQuery } from './utils/hooks/useMediaQuery';
+export { default as useLocalStorage } from './utils/hooks/useLocalStorage';
