8eaa1dfbcd2a0bc0dac0c6deb819191244c4b120
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-03-29 22:13:59 +0530

    Added the changes for the accordian item

--- src/components/accordian/index.tsx
+++ src/components/accordian/index.tsx
@@ -2,10 +2,14 @@ import styled from 'styled-components';
 import colors from '../../utils/theme';
 
 const Accordian = styled.div`
-background-color: ${colors.secondary};
+background-color: ${colors.ternary};
 padding: 6px 12px;
 border-radius: 5px;
 color: ${colors.text_primary};
+&:hover{
+    transition: 0.3s;
+    background-color: ${colors.secondary};
+}
 `;
 
 export default Accordian;
