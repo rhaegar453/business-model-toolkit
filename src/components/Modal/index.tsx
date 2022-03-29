6b0b5bd9e62775d770758d860cb580b12d1433eb
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-03-29 11:12:37 +0530

    Added the changes for the modal content

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,8 +2,10 @@ import React, {
   ReactNode,
 } from 'react';
 import styled, { keyframes } from 'styled-components';
+import colors from '../../utils/theme';
 import { OpacityWithScale } from '../Animations/opacity';
 import Button from '../button';
+import Field from '../field';
 import Portal from '../Portal';
 import Text from '../text';
 
@@ -11,8 +13,9 @@ const ModalBase = styled.div`
 padding: 20px;
 border-radius: 15px;
 margin: 5px;
-background-color: white;
+background-color: ${colors.ternary};
 min-height: 300px;
+min-width: 60%;
 `;
 
 export interface ModalProps{
@@ -23,10 +26,13 @@ export interface ModalProps{
 
 const Modal = ({ isOpen, children, handleClose }:ModalProps) => (!isOpen ? null : (
   <Portal elementId="react-portal-modal-container">
-    <OpacityWithScale>
+    <OpacityWithScale style={{ display: 'flex', justifyContent: 'center', width: '80%' }}>
       <ModalBase>
         <Button size="small" onClick={handleClose}>Close</Button>
-        <Text size="large" style={{ color: '#333333' }}>Hello World this Shivaraj</Text>
+        <Text size="large">Hello World this Shivaraj</Text>
+        <Field>
+          <Field.Input placeholder="Please enter your name" />
+        </Field>
         {children}
       </ModalBase>
     </OpacityWithScale>
--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -1,11 +1,17 @@
-import { ReactNode, useLayoutEffect, useState } from 'react';
+import {
+  ReactNode, useLayoutEffect, useRef, useState,
+} from 'react';
 import { createPortal } from 'react-dom';
 
-const createWrapper = (wrapperId:string) => {
+const createWrapper = (wrapperId:string, handleClick:()=>void) => {
   const wrapperElement = document.createElement('div');
   wrapperElement.setAttribute('id', wrapperId);
   wrapperElement.setAttribute('style', 'background-color:#141313bd;display:flex;justify-content:center;align-items:center;position:absolute;left:0;top:0;width:100%;height:100%;');
   document.body.appendChild(wrapperElement);
+  wrapperElement.onclick = (e) => {
+    handleClick();
+    e.stopPropagation();
+  };
   return wrapperElement;
 };
 
@@ -17,13 +23,17 @@ interface PortalProps{
 const Portal = ({ children, elementId = 'react-portal-element' }:PortalProps) => {
   const [isLoaded, setLoaded] = useState(false);
   const [wrapperElement, setWrapperElement] = useState<any>(null);
+
+  const handleClickEvent = () => {
+    console.log('clicked outside');
+  };
   useLayoutEffect(() => {
     setLoaded(true);
     let element = document.getElementById(elementId);
     let systemCreated = false;
     if (!element) {
       systemCreated = true;
-      element = createWrapper(elementId);
+      element = createWrapper(elementId, handleClickEvent);
     }
     setWrapperElement(element);
     return () => {
